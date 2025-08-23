c5f58a2a3c537d9085a69f92d2e0e7a844280f49
jminchezm <jminchezm@miumg.edu.gt>
2025-08-22 22:50:15 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PIOGHOASIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
